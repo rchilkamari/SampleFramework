9a4906e0fdc779163fb0f64c326a08c8e08b419e
rchil <rchil@DESKTOP-C1KB7NO>
2019-05-02 22:40:53 -0500

    test

--- Sampleframeworkscratch/src/test/java/com/insurance/qa/testcases/LoginPageTest.java
+++ Sampleframeworkscratch/src/test/java/com/insurance/qa/testcases/LoginPageTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.insurance.qa.base.TestBase;
@@ -28,7 +29,7 @@ public class LoginPageTest extends TestBase{
 	}
 	
 
-	@BeforeSuite
+	@BeforeClass
 	
 	public void setUp() {
 		
@@ -50,7 +51,7 @@ public class LoginPageTest extends TestBase{
 		
 	}
 	
-	@Test(priority=2)
+	@Test(priority=4)
 	
 	public void validatelogo() {
 		
@@ -60,7 +61,7 @@ public class LoginPageTest extends TestBase{
 		
 	}
 	
-	@Test(priority=3)
+	@Test(priority=2)
 	
 	public void logoTextTest() {
 		
@@ -70,7 +71,7 @@ public class LoginPageTest extends TestBase{
 		
 	}
 	
-	@Test(priority=4)
+	@Test(priority=3)
 	
 	public void LoginTest() {
 		logger.info("Starting Application");
